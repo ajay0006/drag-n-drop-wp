b594710e087821d8ada16506c484de0da9dac163
damilola <ajay0006@algonquinlive.com>
2023-11-15 11:34:01 -0500

    created basic css and component structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className='App-header'>
+                <div className='drag-n-drop'>
+                    <div className='dnd-table-group'>
+                        <div className='group-title'>Table 1</div>
+                        <div className='dnd-item'>
+                            <di>
+                                <p>Guest 1</p>
+                            </di>
+                        </div>
+                        <div className='dnd-item'>
+                            <di>
+                                <p>Guest 2</p>
+                            </di>
+                        </div>
+                    </div>
+                    <div className='dnd-table-group'>
+                        <div className='group-title'>Table 2</div>
+                        <div className='dnd-item'>
+                            <di>
+                                <p>Guest 1</p>
+                            </di>
+                        </div>
+                        <div className='dnd-item'>
+                            <di>
+                                <p>Guest 2</p>
+                            </di>
+                        </div>
+                    </div>
+                </div>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
